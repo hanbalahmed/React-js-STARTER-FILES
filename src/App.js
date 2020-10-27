a979f8b7fb935f47aec6bc22c42e6ca864309450
hanbalahmed <72215552+hanbalahmed@users.noreply.github.com>
2020-10-27 13:48:01 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>
+        SLACK CLONE <span>🚀🔴🔥🐱‍👤</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
